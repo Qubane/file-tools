95cbbd10a083fb8a1722c0aca516adff515160b1
UltraQbik <no1skill@yandex.ru>
2025-01-28 19:37:10 +0300

    Add some code

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import PyInstaller.__main__
 
 
 COMPILE_DIR = "bin"
@@ -10,7 +11,14 @@ def main():
         os.makedirs(COMPILE_DIR)
 
     for file in os.listdir("source"):
-        pass
+        path = os.path.join("source", file)
+        print(f"Compiling '{path}'")
+        PyInstaller.__main__.run([
+            path,
+            "--onefile",
+            "--noconsole",
+            f"--name {os.path.join(COMPILE_DIR, os.path.splitext(file)[0])}"
+        ])
 
 
 if __name__ == '__main__':
